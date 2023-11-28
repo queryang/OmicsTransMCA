cfe536294f2b94a5acdbcfcd6bdab1978465cd80
WuYang <query_wy@outlook.com>
2023-11-29 00:02:39 +0800

    消融实验更新：train_MCA_Dense_GEP_MUT；

--- model_omics_experiment/model/OmicsTransMCA_predictor/models/__init__.py
+++ model_omics_experiment/model/OmicsTransMCA_predictor/models/__init__.py
@@ -1,5 +1,6 @@
 from .MCA_Dense_GEP_CNV import MCA_OmicsDense_GEP_CNV
 from .MCA_Dense_GEP_CNV_MUT import MCA_OmicsDense_GEP_CNV_MUT
+from .MCA_Dense_GEP_MUT import MCA_OmicsDense_GEP_MUT
 from .TransMCA_Dense_GEP import Conv_TransMCA_GEP
 from .TransMCA_Dense_GEP_CNV import Conv_TransMCA_GEP_CNV
 from .TransMCA_Dense_GEP_CNV_MUT import Conv_TransMCA_GEP_CNV_MUT
@@ -13,5 +14,6 @@ MODEL_FACTORY = {
     'trans_mca_dense_GEP': Conv_TransMCA_GEP,
     'trans_mca_dense_GEP_MUT': Conv_TransMCA_GEP_MUT,
     'mca_dense_GEP_CNV_MUT': MCA_OmicsDense_GEP_CNV_MUT,
-    'mca_dense_GEP_CNV': MCA_OmicsDense_GEP_CNV
+    'mca_dense_GEP_CNV': MCA_OmicsDense_GEP_CNV,
+    'mca_dense_GEP_MUT': MCA_OmicsDense_GEP_MUT,
 }
