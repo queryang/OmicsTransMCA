f869945e0535b1ebbc94c38c477238df501a53dd
WuYang <query_wy@outlook.com>
2023-12-22 11:13:36 +0800

    MCA&MCA_Dense实验代码修改

--- model_omics_experiment/train_MCA_GEP_CNV_MUT.py
+++ model_omics_experiment/train_MCA_GEP_CNV_MUT.py
@@ -301,7 +301,7 @@ if __name__ == "__main__":
 
     train_sensitivity_filepath = 'data/drug_sensitivity_MixedSet_train.csv'
     test_sensitivity_filepath = 'data/drug_sensitivity_MixedSet_test.csv'
-    gep_filepath = 'data/OmicsExpressionProteinCodingGenesTPMLogp1-23Q2_Only_MEDICUS_GSVA.csv'
+    gep_filepath = 'data/GeneExp_Wilcoxon_test_Analysis_Log10_P_value_C2_KEGG_MEDICUS.csv'
     cnv_filepath = 'data/CNV_Cardinality_analysis_of_variance_Latest_MEDICUS.csv'
     mut_filepath = 'data/MUT_cardinality_analysis_of_variance_Only_MEDICUS.csv'
     smi_filepath = 'data/ccle-gdsc.smi'
@@ -309,7 +309,7 @@ if __name__ == "__main__":
     smiles_language_filepath = 'data/smiles_language/tokenizer_customized'
     model_path = 'result/model'
     params_filepath = 'data/params/MCA_GEP_CNV_MUT.json'
-    training_name = 'MCA_GEP_CNV(Cardinality_Analysis)_MUT_MEDICUS619'
+    training_name = 'MCA_GEP(Log10_P_value)_CNV(Cardinality_Analysis)_MUT_MEDICUS619'
     # run the training
     main(
         train_sensitivity_filepath,
--- model_omics_experiment/train_MCA_GEP_MUT.py
+++ model_omics_experiment/train_MCA_GEP_MUT.py
@@ -297,14 +297,14 @@ if __name__ == "__main__":
 
     train_sensitivity_filepath = 'data/drug_sensitivity_MixedSet_train.csv'
     test_sensitivity_filepath = 'data/drug_sensitivity_MixedSet_test.csv'
-    gep_filepath = 'data/OmicsExpressionProteinCodingGenesTPMLogp1-23Q2_Only_MEDICUS_GSVA.csv'
+    gep_filepath = 'data/GeneExp_Wilcoxon_test_Analysis_Log10_P_value_C2_KEGG_MEDICUS.csv'
     mut_filepath = 'data/MUT_cardinality_analysis_of_variance_Only_MEDICUS.csv'
     smi_filepath = 'data/ccle-gdsc.smi'
     gene_filepath = 'data/MUDICUS_Omic_619_pathways.pkl'
     smiles_language_filepath = 'data/smiles_language/tokenizer_customized'
     model_path = 'result/model'
     params_filepath = 'data/params/MCA_GEP_MUT.json'
-    training_name = 'MCA_GEP_CNV(Cardinality_Analysis)_MUT_MEDICUS619'
+    training_name = 'MCA_GEP(Log10_P_value)_CNV(Cardinality_Analysis)_MUT_MEDICUS619'
     # run the training
     main(
         train_sensitivity_filepath,
